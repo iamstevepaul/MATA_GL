kl div fixed

CustomPolicies.py
@@ -1,6 +1,8 @@
 """
 Author: Steve Paul 
 Date: 1/18/22 """
+import torch
+
 from stable_baselines_al.common.policies import BasePolicy
 import torch as th
 import gym
@@ -87,7 +89,7 @@ class ActorCriticGCAPSPolicy(BasePolicy):
         optimizer_class: Type[th.optim.Optimizer] = th.optim.Adam,
         optimizer_kwargs: Optional[Dict[str, Any]] = None,
         features_dim = 64,
-        node_dim=4
+        node_dim=5
                  ):
         super(ActorCriticGCAPSPolicy, self).__init__(observation_space,
             action_space,
@@ -184,10 +186,10 @@ class ActorCriticGCAPSPolicy(BasePolicy):
         ## From the logits compute the probabilities by clipping, masking and softmax
         # if self.tanh_clipping > 0:
         #     logits = th.tanh(logits) * self.tanh_clipping
-        # if self.mask_logits:
-        #     logits[th.tensor(mask[:,:,:].reshape(logits.shape), dtype=th.bool)] = -math.inf
-        if mask[0, 0,0] == 1:
-            logits[:,:,0] = -math.inf
+        if self.mask_logits:
+            logits[th.tensor(mask[:,:,:].reshape(logits.shape), dtype=th.bool)] = -math.inf
+        # if mask[0, 0,0] == 1:
+        #     logits[:,:,0] = -math.inf
             # print("depot masked")
         # print("Shape of logits: ", logits.shape)
         return logits, glimpse.squeeze(-2)
@@ -237,7 +239,7 @@ class ActorCriticGCAPSPolicy(BasePolicy):
         features, graph_embed = self.extract_features(obs)
 
         latent_pi, values = self.context_extractor(graph_embed, obs)
-        mean_actions = self.decode_action_probabilites(latent_pi,graph_embed, features, obs)[0,:]
+        mean_actions = self.decode_action_probabilites(latent_pi,graph_embed, features, obs)[:,0,:]
         # mean_actions = self.action_net(latent_pi)
         latent_sde = latent_pi
         # if self.sde_features_extractor is not None:
@@ -264,11 +266,12 @@ class ActorCriticGCAPSPolicy(BasePolicy):
         return distribution, values
 
     def context_extractor(self, graph_embed, observations):
-        agent_taking_decision = observations['agent_taking_decision']
+        agent_taking_decision = observations['agent_taking_decision'][:,0].to(torch.int64)
+        n_data = agent_taking_decision.shape[0]
         context = graph_embed[:, None, :] \
                + \
                   self.full_context_nn(
-                      th.cat((self.agent_decision_context(observations['agents_graph_nodes'][:, agent_taking_decision, :]),
+                      th.cat((self.agent_decision_context(observations['agents_graph_nodes'][torch.arange(0,n_data), agent_taking_decision, :][:,None,:]),
                               self.agent_context(observations['agents_graph_nodes']).sum(1)[:,None,:]), -1))
         return context, self.value_net(context)
         # return context, self.value_net(th.cat((self.agent_decision_context(observations['agent_taking_decision_coordinates']),

GCAPCN.py
@@ -69,20 +69,20 @@ class GCAPCNFeatureExtractorNTDA(nn.Module):
         self.activ = nn.Tanh()
 
     def forward(self, data, mask=None):
-        active_tasks = ((data['nodes_visited'] == 0).nonzero())[:, 1]
+        # active_tasks = ((data['nodes_visited'] == 0).nonzero())[:, 1]
         # print("Active tasks before node embedding: ",active_tasks)
         X = data['task_graph_nodes']
-        X_loc = X[:, active_tasks[1:] - 1, :]
-        distance_matrix = ((((X_loc[:, :, None] - X_loc[:, None]) ** 2).sum(-1)) ** .5)
+        X_loc = X
+        # distance_matrix = ((((X_loc[:, :, None] - X_loc[:, None]) ** 2).sum(-1)) ** .5)
         # distance_matrix = torch.cdist(X_loc, X_loc)
         num_samples, num_locations, _ = X_loc.size()
         # A = ((1 / distance_matrix) * (torch.eye(num_locations, device=distance_matrix.device).expand(
         #     (num_samples, num_locations, num_locations)) - 1).to(torch.bool).to(torch.float))
         # A[A != A] = 0
-        A = 1 / (1 + distance_matrix)
-        A = A * (distance_matrix > 0).to(torch.float32)
+        # A = 1 / (1 + distance_matrix)
+        A = data['task_graph_adjacency']
         # A = data['task_graph_adjacency']
-        D = torch.mul(torch.eye(num_locations, device=distance_matrix.device).expand((num_samples, num_locations, num_locations)),
+        D = torch.mul(torch.eye(num_locations, device=X.device).expand((num_samples, num_locations, num_locations)),
                       (A.sum(-1) - 1)[:, None].expand((num_samples, num_locations, num_locations)))
 
 

mrta_flood_env.py
@@ -105,12 +105,12 @@ class MRTAENV(Env):
                 #                                                            self.agent_taking_decision, :].reshape(1,
                 #                                                                                                   2).shape),
                 # topo_laplacian=Box(low=0, high=100000, shape=(n_locations-1,n_locations-1)),
-                task_graph_nodes=Box(low=0, high=1, shape=(n_locations-1,4)),
-                # task_graph_adjacency=Box(low=0, high=1, shape=(n_locations-1, n_locations-1)),
+                task_graph_nodes=Box(low=0, high=1, shape=(n_locations-1,5)),
+                task_graph_adjacency=Box(low=0, high=1, shape=(n_locations-1, n_locations-1)),
                 agents_graph_nodes=Box(low=0, high=1, shape=(n_agents, 5)),
-                # agents_graph_adjacency=Box(low=0, high=1, shape=(n_agents, n_agents)),
+                agents_graph_adjacency=Box(low=0, high=1, shape=(n_agents, n_agents)),
                 nodes_visited=Box(low=0, high=1, shape=self.nodes_visited.shape),
-                agent_taking_decision=Discrete(n_agents),
+                agent_taking_decision=Box(low=0, high=n_agents, shape=(1,), dtype=int),
                 first_dec = MultiBinary(1)
             ))
 
@@ -146,9 +146,9 @@ class MRTAENV(Env):
             #                                                                             2),
             # 'topo_laplacian':self.topo_laplacian,
             'task_graph_nodes': task_graph_nodes,
-            # 'task_graph_adjacency':task_graph_adjacency,
+            'task_graph_adjacency':task_graph_adjacency,
             'agents_graph_nodes':agents_graph_nodes,
-            # 'agents_graph_adjacency':agents_graph_adjacency,
+            'agents_graph_adjacency':agents_graph_adjacency,
             'nodes_visited':self.nodes_visited,
             'first_dec': self.first_dec,
             'agent_taking_decision': self.agent_taking_decision
@@ -218,7 +218,7 @@ class MRTAENV(Env):
     def step(self, action):
         # print("Time: ", self.time)
         # print("New action taken from the available list: ", action)
-        action = self.active_tasks[action]
+        # action = self.active_tasks[action]
         # print("Actual action: ", action)
         self.episode_start = 0
         self.first_dec = False
@@ -280,7 +280,7 @@ class MRTAENV(Env):
         # print("Active tasks before update: ", self.active_tasks)
         self.active_tasks = ((self.nodes_visited == 0).nonzero())[0]
         # print("Active tasks after update: ", self.active_tasks)
-
+        # print(sum(self.nodes_visited))
         if sum(self.nodes_visited) == self.n_locations - 1:
             # reward = 1/(self.total_distance_travelled + 10e-5) - (self.total_length - self.n_locations+1)/self.n_locations
             # 1/(self.total_distance_travelled**2+ 10e-5)## change this with the distance travelled
@@ -347,9 +347,10 @@ class MRTAENV(Env):
         locations = torch.tensor(self.locations)
         time_deadlines = (self.time_deadlines.T)
         location_demand = (self.location_demand.T)
-        node_properties = torch.cat((locations, time_deadlines, location_demand), dim=1)
+        deadlines_passed = self.deadline_passed.T
+        node_properties = torch.cat((locations, time_deadlines, location_demand, deadlines_passed), dim=1)
         node_properties = node_properties[1:, :] # excluding the depot
-        node_properties = node_properties/node_properties.max(dim=0).values # normalizing
+        node_properties[:, 0:4] = node_properties[:, 0:4]/node_properties[:, 0:4].max(dim=0).values # normalizing all except deadline_passed
         distance_matrix = torch.cdist(node_properties, node_properties)
         adjacency_matrix = 1/(1+torch.cdist(node_properties, node_properties))
         adjacency_matrix = adjacency_matrix*(distance_matrix>0).to(torch.float32) # setting diagonal elements as 0